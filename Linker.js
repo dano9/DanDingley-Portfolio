e575a50ba67d561dc92a06157c713eed286d1a29
dano9 <42714883+dano9@users.noreply.github.com>
2023-11-15 18:03:25 +0000

    Updated Linker.js to work with mobile devices

--- Linker.js
+++ Linker.js
@@ -6,8 +6,6 @@ var isMobile = (navigator.userAgent.match(/Android/i) || navigator.userAgent.mat
 var time = 0;
 function Loop()
 {
-    
-
     time += 0.05;
     GameLoop();
     PVUpdate();
@@ -28,34 +26,48 @@ window.addEventListener("resize", () => {
 });
 function Resize()
 {
-    GameResize();
-    mainContent.style.minWidth = "10px";
-    mainContent.style.width = Math.min(window.innerWidth * 0.48, 1000) + "px";
     let mCRect = mainContent.getBoundingClientRect();
-    if (!isMobile) {mainContent.style.marginLeft = ((window.innerWidth - mCRect.width) * 0.5) + "px";}
+    portraitProjV = mCRect.width < 500;
+    GameResize();
+    PVResize();
+   // mainContent.style.minWidth = "10px";
+    if (isMobile)
+    {
+        //mainContent.style.width = Math.min(window.innerWidth * 0.48, 1000) + "px";
+    }
+    
+    //if (!isMobile) 
+    {mainContent.style.marginLeft = ((window.innerWidth - mCRect.width) * 0.5) + "px";}
+    //else
+    //{mainContent.style.marginLeft = ((window.innerWidth - mCRect.width) * 0.25) + "px";}
     //else {mainContent.style.padding = "15px";}
     //console.log(mainContent.style.maxWidth);
-    mainContent.style.padding = "50px";
+    //mainContent.style.padding = "50px";
+
+    
+    console.log("portrait:" + portraitProjV);
 
     gameHud.style.maxWidth = Math.min(Math.max(window.innerWidth * 0.9, 200), 1000) + "px";
     gameHud.style.width = Math.min(Math.max(window.innerWidth * 0.6, 200), 1000) + "px";
     let gHRect = gameHud.getBoundingClientRect();
-    gameHud.style.marginTop = (mCRect.height + (window.innerHeight * 0.8)) + "px";
-    if (!isMobile) {gameHud.style.marginLeft = ((window.innerWidth - gHRect.width) * 0.5) + "px";}
+    gameHud.style.marginTop = (mCRect.height + (window.innerHeight * 1.8)) + "px";
+    //if (!isMobile) 
+    //{gameHud.style.marginLeft = ((window.innerWidth - gHRect.width) * 0.5) + "px";}
 
-    let pad = Math.round(0.04 * gHRect.width );
-    if (!isMobile) {pad *= 0.8;}
+    /*let pad = Math.round(0.04 * gHRect.width );
+    //if (!isMobile) 
+    {pad *= 0.8;}
 
     document.getElementById("wormLength").style.fontSize = pad + "px";
     document.getElementById("wormLength").style.paddingLeft = 1 + "px";
     if (isMobile)
     {
-        document.getElementById("wormLength").style.paddingLeft = 10 + "px";
+        //document.getElementById("wormLength").style.paddingLeft = 10 + "px";
     }
 
     document.getElementById("highScore").style.fontSize = pad + "px";
     document.getElementById("highScore").style.paddingLeft = 0 + "px";
 
     document.getElementById("wormRound").style.fontSize = pad + "px";
-    document.getElementById("wormRound").style.paddingLeft = 0 + "px";
+    document.getElementById("wormRound").style.paddingLeft = 0 + "px";*/
 }
